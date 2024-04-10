d63e1118e029a4f60c1f1ae33c944263fbea02b4
callmecposu <pc.cposu@gmail.com>
2024-04-10 15:14:07 -0400

    #server: added getCourses with optional full-text search

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,7 @@ import getCourse from "./services/course/getCourse";
 import createLecture from "./services/lecture/createLecture";
 import updateLecture from "./services/lecture/updateLecture";
 import deleteLecture from "./services/lecture/deleteLecture";
+import getCourses from "./services/course/getCourses";
 require("dotenv").config();
 const cors = require("cors");
 
@@ -34,6 +35,7 @@ app.post("/create_user", createUser);
 app.post("/login", login);
 app.get("/get_user_from_jwt", withUser, getUserFromJwt);
 
+app.get('/courses', getCourses)
 app.get("/course/:id", getCourse);
 app.post("/create_course", withUser, createCourse);
 app.post("/update_course/:id", withUser, updateCourse);
